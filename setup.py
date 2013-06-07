8a36fced8af8aef3e4e81f1cacf936bbab263c1d
Éric Araujo <earaujo@ludia.com>
2013-06-07 18:55:39 -0400

    Move test dependencies out of setup-requires

--- setup.py
+++ setup.py
@@ -1,20 +1,6 @@
 import setuptools
 
-setup_requires = [
-    'd2to1',
-    'coverage',
-    'mock',
-    'nose',
-    'nosexcover',
-    'webtest',
-    'yanc',
-    ]
-
-dependency_links = [
-    ]
-
 setuptools.setup(
-    setup_requires=setup_requires,
+    setup_requires=['d2to1'],
     d2to1=True,
-    dependency_links=dependency_links,
-    )
+)

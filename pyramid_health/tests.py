3798995d12bcfcc132766e77f7be7166be958dc3
Pior Bastida <pbastida@ludia.com>
2013-08-16 15:33:29 -0400

    Add option to enable maintenance mode when a file exists

--- pyramid_health/tests.py
+++ pyramid_health/tests.py
@@ -1,4 +1,5 @@
 import unittest
+import mock
 
 import webtest
 from pyramid.config import Configurator
@@ -22,6 +23,38 @@ class TestHealth(unittest.TestCase):
 
     def test_post(self):
         response = self.app.post('/health')
+
+        self.assertEqual(response.status_int, 200)
+        self.assertEqual(response.body, b'OK')
+
+
+class TestHealthMaintenance(unittest.TestCase):
+
+    maxDiff = None
+
+    def setUp(self):
+        settings = {'health.maintenance.file': '/maintenance/file/test'}
+        config = Configurator(settings=settings)
+        config.include('pyramid_health')
+        self.app = webtest.TestApp(config.make_wsgi_app())
+
+    @mock.patch("os.path.exists")
+    def test_get_maintenance_on(self, m_exists):
+        m_exists.return_value = True
+
+        response = self.app.get('/health', expect_errors=True)
+
+        m_exists.assert_called_with('/maintenance/file/test')
+
+        self.assertEqual(response.status_int, 503)
+
+    @mock.patch("os.path.exists")
+    def test_get_maintenance_off(self, m_exists):
+        m_exists.return_value = False
+
+        response = self.app.get('/health')
+
+        m_exists.assert_called_with('/maintenance/file/test')
 
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.body, b'OK')
--- pyramid_health/views.py
+++ pyramid_health/views.py
@@ -1,5 +1,8 @@
+import os
+
 from pyramid.view import view_config
 from pyramid.response import Response
+from pyramid.httpexceptions import HTTPServiceUnavailable
 
 
 @view_config(route_name='healthcheck')
@@ -8,4 +11,10 @@ def health(request):
 
     For human-friendliness, a body containing 'OK' is also returned.
     """
+    settings = request.registry.settings
+
+    if 'health.maintenance.file' in settings:
+        if os.path.exists(settings['health.maintenance.file']):
+            return HTTPServiceUnavailable()
+
     return Response('OK', content_type='text/plain')
